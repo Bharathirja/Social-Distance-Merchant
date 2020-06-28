cc4832ad4eaaeae0e47627c0ba181a14b24b2867
Bharathiraja SS <54929507+BharathirajaSS@users.noreply.github.com>
2020-06-28 18:00:06 +0530

    Profile

--- Screens/profile.js
+++ Screens/profile.js
@@ -1,17 +1,52 @@
 
-import React from 'react';
-import { View, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-function ProfileDetails() {
-  return (
-    <View>
-      <Text>Hello Profile</Text>
-    </View>
-  )
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+ function ProfileDetails () {
+
+    return (
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.headerContent}>
+                <Image style={styles.avatar}
+                  source={{uri: 'https://bootdey.com/img/Content/avatar/avatar1.png'}}/>
+
+                <Text style={styles.name}>
+                  John Doe
+                </Text>
+            </View>
+          </View>
+
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.textInfo}>
+                johndoe@gmail.com
+              </Text>
+          
+              <Text style={styles.textInfo}>
+                Mobile Number: +9123456786
+              </Text>
+            
+              <Text style={styles.textInfo}>
+                Location: Chennai 
+              </Text>
+            </View>
+        </View>
+      </View>
+    );
+  
 }
+
+
 
 export default function ProfileScreen() {
     return (
@@ -28,3 +63,37 @@ export default function ProfileScreen() {
       </Stack.Navigator>
     );
   }
+
+  const styles = StyleSheet.create({
+    header:{
+      backgroundColor: "#1E90FF",
+    },
+    headerContent:{
+      padding:30,
+      alignItems: 'center',
+    },
+    avatar: {
+      width: 130,
+      height: 130,
+      borderRadius: 63,
+      borderWidth: 4,
+      borderColor: "white",
+      marginBottom:10,
+    },
+    name:{
+      fontSize:22,
+      color:"#FFFFFF",
+      fontWeight:'600',
+    },
+    bodyContent: {
+      flex: 1,
+      alignItems: 'center',
+      padding:30,
+    },
+    textInfo:{
+      fontSize:18,
+      marginTop:20,
+      color: "#696969",
+    }
+  });
+   
